1f4a159a010b626756bca90a2cc2c3c4ecdc53ae
Brian <Brian@utnso40>
2015-08-05 14:29:49 -0300

    comento header de sockets

--- commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
+++ commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
@@ -20,32 +20,104 @@
 #ifndef CLIENTE_SERVIDOR_H_
 #define CLIENTE_SERVIDOR_H_
 
+
 void validarError(int validar, char* mensaje);
 
+/**
+* @NAME: crearSocket
+* @DESC: crea un socket y retorna su descriptor
+* @PARAMS:
+*/
 int crearSocket();
 
+/**
+* @NAME: crearSocket
+* @DESC: asocia un puerto con un socket
+* @PARAMS:
+* 	socket que se quiere asociar
+* 	puerto al que se lo va a asociar
+*/
 void asociarAPuerto(int socket, int puerto);
 
+
+/**
+* @NAME: liberarSocket
+* @DESC: crea un socket y retorna su descriptor
+* @PARAMS:
+*/
 void liberarSocket(int socket);
+
+
+/**
+* @NAME: obviarErrorDeIpEnUso
+* @DESC: No toma en cuenta el error que sale cuando se uso un socket anteriormente
+* @PARAMS:
+*/
 
 void obviarErrorDeIpEnUso(int socket);
 
+/**
+* @NAME: conectarAServidor
+* @DESC: conecta un socket cliente a un socket servidor
+* @PARAMS:
+*/
+
 void conectarAServidor(int socket,char* direccionIp,int puerto);
 
+/**
+* @NAME: enviarMensaje
+* @DESC: envia el mensaje "buffer" de un tamanio "tamanioBuffer"
+* @PARAMS:
+*/
 int enviarMensaje(int socketCliente, void* buffer,int tamanioBuffer);
 
+/**
+* @NAME: recibirMensaje
+* @DESC: recibe el mensaje "buffer" de un tamanio "tamanioBuffer"
+* @PARAMS:
+*/
 int recibirMensaje(int socketCliente, void* buffer,int tamanioBuffer);
 
+/**
+* @NAME: recibirMensajeCompleto
+* @DESC: se bloquea hasta que recibe la cantidad de bytes de "tamanioBuffer"
+* @PARAMS:
+*/
 int recibirMensajeCompleto(int socketCliente, void* buffer,int tamanioBuffer);
 
+/**
+* @NAME: escucharConexiones
+* @DESC: bloquea el proceso hasta que haya una solicitud de conexion
+* @PARAMS:
+*/
 void escucharConexiones(int socket, int maximasConexionesEntrantes);
 
+/**
+* @NAME: crearSocketParaAceptarSolicitudes
+* @DESC: retorna el descriptor del respectivo socket que se conecto al servidor
+* @PARAMS:
+*/
 int crearSocketParaAceptarSolicitudes(int socketServidor);
 
+/**
+* @NAME: recibirBloque
+* @DESC: recibe un bloque completo de datos de tamaño tamanioBloque
+* @PARAMS:
+*/
 void* recibirBloque(size_t* tamanioBloque,int socketEnviador);
 
+/**
+* @NAME: serializarIntYCadena
+* @DESC: guarda en el bloque "bloque" el int "entero" y la cadena "cadena"
+* @PARAMS:
+*/
 void serializarIntYCadena(int entero,char* cadena,void* bloque);
 
+/**
+* @NAME: deserializarIntYCadena
+* @DESC: guarda una cadena y un int recibidos en un bloque de memoria
+* @PARAMS:
+*/
 void deserializarIntYCadena(int* entero,char* cadena,int tamanioBloque,void* bloque);
 
 #endif /* CLIENTE_SERVIDOR_H_ */
