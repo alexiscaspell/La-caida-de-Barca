fb39edc40132205da6726a5240e9d85f799d645a
alexiscaspell <alexiscaspell@gmail.com>
2015-08-30 16:19:03 -0300

    Actualizo comentarios de headers de sockets

--- commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
+++ commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
@@ -20,7 +20,11 @@
 #ifndef CLIENTE_SERVIDOR_H_
 #define CLIENTE_SERVIDOR_H_
 
-
+/**
+* @NAME: validarError
+* @DESC: verifica si se produjo un error, en caso afirmativo retorna la corespondiente excepcion
+* @PARAMS:
+*/
 void validarError(int validar, char* mensaje);
 
 /**
@@ -31,7 +35,7 @@ void validarError(int validar, char* mensaje);
 int crearSocket();
 
 /**
-* @NAME: crearSocket
+* @NAME: asociarAPuerto
 * @DESC: asocia un puerto con un socket
 * @PARAMS:
 * 	socket que se quiere asociar
@@ -44,6 +48,7 @@ void asociarAPuerto(int socket, int puerto);
 * @NAME: liberarSocket
 * @DESC: crea un socket y retorna su descriptor
 * @PARAMS:
+* 	socket a liberar
 */
 void liberarSocket(int socket);
 
@@ -52,6 +57,7 @@ void liberarSocket(int socket);
 * @NAME: obviarErrorDeIpEnUso
 * @DESC: No toma en cuenta el error que sale cuando se uso un socket anteriormente
 * @PARAMS:
+* 	socket en uso
 */
 
 void obviarErrorDeIpEnUso(int socket);
@@ -60,6 +66,9 @@ void obviarErrorDeIpEnUso(int socket);
 * @NAME: conectarAServidor
 * @DESC: conecta un socket cliente a un socket servidor
 * @PARAMS:
+* 	socket en uso
+* 	ip del servidor a conectarse
+* 	puerto en el que opera el servidor
 */
 
 void conectarAServidor(int socket,char* direccionIp,int puerto);
@@ -68,6 +77,9 @@ void conectarAServidor(int socket,char* direccionIp,int puerto);
 * @NAME: enviarMensaje
 * @DESC: envia el mensaje "buffer" de un tamanio "tamanioBuffer"
 * @PARAMS:
+* 	socket en uso
+* 	bloque de bytes que se enviaran
+* 	tamaño del bloque de bytes
 */
 int enviarMensaje(int socketCliente, void* buffer,int tamanioBuffer);
 
@@ -82,6 +94,9 @@ int recibirMensaje(int socketCliente, void* buffer,int tamanioBuffer);
 * @NAME: recibirMensajeCompleto
 * @DESC: se bloquea hasta que recibe la cantidad de bytes de "tamanioBuffer"
 * @PARAMS:
+* 	socket en uso
+* 	bloque de bytes que se recibiran
+* 	tamaño del bloque de bytes
 */
 int recibirMensajeCompleto(int socketCliente, void* buffer,int tamanioBuffer);
 
@@ -89,6 +104,9 @@ int recibirMensajeCompleto(int socketCliente, void* buffer,int tamanioBuffer);
 * @NAME: escucharConexiones
 * @DESC: bloquea el proceso hasta que haya una solicitud de conexion
 * @PARAMS:
+* 	socket en uso
+* 	bloque de bytes que se recibiran
+* 	tamaño del bloque de bytes
 */
 void escucharConexiones(int socket, int maximasConexionesEntrantes);
 
@@ -96,6 +114,8 @@ void escucharConexiones(int socket, int maximasConexionesEntrantes);
 * @NAME: crearSocketParaAceptarSolicitudes
 * @DESC: retorna el descriptor del respectivo socket que se conecto al servidor
 * @PARAMS:
+* 	socket en uso
+* 	cantidad maxima de conexiones que podra atender
 */
 int crearSocketParaAceptarSolicitudes(int socketServidor);
 
@@ -103,6 +123,8 @@ int crearSocketParaAceptarSolicitudes(int socketServidor);
 * @NAME: recibirBloque
 * @DESC: recibe un bloque completo de datos de tamaño tamanioBloque
 * @PARAMS:
+* 	tamaño del bloque que se recibira
+* 	socket del enviador del bloque
 */
 void* recibirBloque(size_t* tamanioBloque,int socketEnviador);
 
@@ -110,6 +132,9 @@ void* recibirBloque(size_t* tamanioBloque,int socketEnviador);
 * @NAME: serializarIntYCadena
 * @DESC: guarda en el bloque "bloque" el int "entero" y la cadena "cadena"
 * @PARAMS:
+* 	int que se quiere serializar
+* 	cadena a serializar
+* 	puntero al bloque en el cual se quiere copiar el int y el string
 */
 void serializarIntYCadena(int entero,char* cadena,void* bloque);
 
@@ -117,6 +142,9 @@ void serializarIntYCadena(int entero,char* cadena,void* bloque);
 * @NAME: deserializarIntYCadena
 * @DESC: guarda una cadena y un int recibidos en un bloque de memoria
 * @PARAMS:
+* 	int que se quiere serializar
+* 	cadena a deserializar
+* 	puntero al bloque en el cual se quiere extraer el int y el string
 */
 void deserializarIntYCadena(int* entero,char* cadena,int tamanioBloque,void* bloque);
 
