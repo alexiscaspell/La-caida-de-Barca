05869a2199525fc1b8366f075b8812424a69f5ae
Ronan Camargo <ronan.cam@gmail.com>
2015-09-05 20:48:57 -0300

    tuvieja

--- SWAP/src/SWAP.c
+++ SWAP/src/SWAP.c
@@ -6,9 +6,14 @@ int main(void) {
 
 	tipoConfigSWAP* configuracion = cargarArchivoDeConfiguracionDeSWAP("/home/utnso/Escritorio/cfgSWAP");
 
-
+	puts("tuvieja");
+	puts("tujavie");
+	puts("tujavier");
 
 	destruirConfigSWAP(configuracion);
+
+
+	puts("No se permiten gorens, solo se permite uno.");
 
 	return EXIT_SUCCESS;
 }
