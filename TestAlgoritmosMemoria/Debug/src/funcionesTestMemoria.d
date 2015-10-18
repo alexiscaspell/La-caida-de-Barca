2d6a9cc68a24b167f1841810f4ca13411adb6faf
brianwolf <brian.d.lobo@gmail.com>
2015-10-18 13:50:14 -0300

    subo funciones para el manejo de los algoritmos

---
+++ TestAlgoritmosMemoria/Debug/src/funcionesTestMemoria.d
@@ -0,0 +1,13 @@
+src/funcionesTestMemoria.d: ../src/funcionesTestMemoria.c \
+ ../src/funcionesTestMemoria.h \
+ /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h \
+ /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h \
+ /home/utnso/git/tp-2015-2c-los-javimancos/commons/string.h
+
+../src/funcionesTestMemoria.h:
+
+/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h:
+
+/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
+
+/home/utnso/git/tp-2015-2c-los-javimancos/commons/string.h:
---
+++ TestAlgoritmosMemoria/src/TestAlgoritmosMemoria.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <commonsDeAsedio/estructuras.h>
+
+
+int main(void) {
+
+
+
+
+	return EXIT_SUCCESS;
+}
