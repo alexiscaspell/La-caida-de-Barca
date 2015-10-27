641a6063adc9f24ce9d6c8704f79b25d9c7d6e2f
alexiscaspell <alexiscaspell@gmail.com>
2015-10-27 16:07:56 -0300

    Subo update de tests

--- MemoriaTestSuite/Debug/src/Tests.d
+++ MemoriaTestSuite/Debug/src/Tests.d
@@ -1,17 +1,5 @@
-src/Tests.d: ../src/Tests.c ../src/Tests.h ../src/funcionesMemoria.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
+src/Tests.d: ../src/Tests.c ../src/Tests.h ../src/funcionesMemoria.h
 
 ../src/Tests.h:
 
 ../src/funcionesMemoria.h:
-
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h:
-
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
-
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h:
-
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
--- MemoriaTestSuite/Debug/src/funcionesMemoria.d
+++ MemoriaTestSuite/Debug/src/funcionesMemoria.d
@@ -1,16 +1,16 @@
 src/funcionesMemoria.d: ../src/funcionesMemoria.c \
  ../src/funcionesMemoria.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h \
- /home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
+ /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h \
+ /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h \
+ /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h \
+ /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
 
 ../src/funcionesMemoria.h:
 
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h:
+/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h:
 
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
+/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
 
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h:
+/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/estructuras.h:
 
-/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
+/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
